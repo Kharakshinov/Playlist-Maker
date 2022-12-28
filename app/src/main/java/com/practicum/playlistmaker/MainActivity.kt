d452fd728e51cea3c96f55a6ea14561d01efbe23
Дмитрий Харакшинов <d.kharakshinov@mail.ru>
2022-12-28 14:02:01 +0300

    Playlist Maker. Спринт 8. Попытка 2.

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -3,7 +3,6 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +13,10 @@ class MainActivity : AppCompatActivity() {
         val buttonMediaLibrary = findViewById<Button>(R.id.button_media_library)
         val buttonSettings = findViewById<Button>(R.id.button_settings)
 
-        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val displaySearch = Intent(this@MainActivity, Search::class.java)
-                startActivity(displaySearch)
-            }
+        buttonSearch.setOnClickListener {
+            val displaySearch = Intent(this, Search::class.java)
+            startActivity(displaySearch)
         }
-        buttonSearch.setOnClickListener(imageClickListener)
 
         buttonMediaLibrary.setOnClickListener {
             val displayMediaLibrary = Intent(this, MediaLibrary::class.java)
