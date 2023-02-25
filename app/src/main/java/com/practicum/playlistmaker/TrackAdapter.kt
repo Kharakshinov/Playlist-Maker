5418e4c97048640eada8f5e3ea5f8bc1f133d6b1
Дмитрий Харакшинов <d.kharakshinov@mail.ru>
2023-02-25 10:55:25 +0300

    Спринт 10. Промежуточный результат. Необходима доработка.

---
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,24 @@
+package com.practicum.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.model.Track
+
+class TrackAdapter(
+    private val data: List<Track>
+): RecyclerView.Adapter<TrackViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
---
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,43 @@
+package com.practicum.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.model.Track
+
+class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
+    private val photo: ImageView = itemView.findViewById(R.id.photoImageView)
+    private val track: TextView = itemView.findViewById(R.id.track_name_TextView)
+    private val author: TextView = itemView.findViewById(R.id.author_name)
+    private val length: TextView = itemView.findViewById(R.id.track_length)
+    private val ellipse: ImageView = itemView.findViewById(R.id.icon_ellipse)
+    private val iconForward: ImageView = itemView.findViewById(R.id.icon_forward)
+
+
+    fun bind (item: Track){
+        track.text = item.trackName
+        length.text = item.trackTime
+        author.text = item.artistName
+
+        Glide.with(itemView)
+            .load(item.artworkUrl100)
+            .centerInside()
+            .placeholder(R.drawable.icon_no_reply)
+            .transform(RoundedCorners(10))
+            .into(photo)
+
+        Glide.with(itemView)
+            .load(R.drawable.icon_ellipse)
+            .centerInside()
+            .into(ellipse)
+
+        Glide.with(itemView)
+            .load(R.drawable.icon_arrow_forward)
+            .centerInside()
+            .into(iconForward)
+
+    }
+}
