a516c67e95ff51a2b08991519732bfb6e55630ee
Дмитрий Харакшинов <d.kharakshinov@mail.ru>
2023-06-09 17:01:21 +0300

    Спринт 16. Добавил на Главный экран роутер.

--- app/src/main/java/com/practicum/playlistmaker/mainscreen/presentation/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/mainscreen/presentation/MainActivity.kt
@@ -1,13 +1,9 @@
-package com.practicum.playlistmaker.mainscreen
+package com.practicum.playlistmaker.mainscreen.presentation
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.practicum.playlistmaker.R
-import com.practicum.playlistmaker.medialibrary.MediaLibrary
-import com.practicum.playlistmaker.search.presentation.SearchActivity
-import com.practicum.playlistmaker.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,20 +12,18 @@ class MainActivity : AppCompatActivity() {
         val buttonSearch = findViewById<Button>(R.id.button_search)
         val buttonMediaLibrary = findViewById<Button>(R.id.button_media_library)
         val buttonSettings = findViewById<Button>(R.id.button_settings)
+        val router = MainScreenRouter(this)
 
         buttonSearch.setOnClickListener {
-            val displaySearch = Intent(this, SearchActivity::class.java)
-            startActivity(displaySearch)
+            router.openSearch()
         }
 
         buttonMediaLibrary.setOnClickListener {
-            val displayMediaLibrary = Intent(this, MediaLibrary::class.java)
-            startActivity(displayMediaLibrary)
+            router.openMediaPlayer()
         }
 
         buttonSettings.setOnClickListener {
-            val displaySettings = Intent(this, SettingsActivity::class.java)
-            startActivity(displaySettings)
+           router.openSettings()
         }
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/mainscreen/presentation/MainScreenRouter.kt
@@ -0,0 +1,27 @@
+package com.practicum.playlistmaker.mainscreen.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.practicum.playlistmaker.medialibrary.MediaLibrary
+import com.practicum.playlistmaker.search.presentation.SearchActivity
+import com.practicum.playlistmaker.settings.SettingsActivity
+
+class MainScreenRouter(
+    private val activity: AppCompatActivity
+) {
+
+    fun openSearch(){
+        val displaySearch = Intent(activity, SearchActivity::class.java)
+        activity.startActivity(displaySearch)
+    }
+
+    fun openMediaPlayer(){
+        val displayMediaLibrary = Intent(activity, MediaLibrary::class.java)
+        activity.startActivity(displayMediaLibrary)
+    }
+
+    fun openSettings(){
+        val displaySettings = Intent(activity, SettingsActivity::class.java)
+        activity.startActivity(displaySettings)
+    }
+}
