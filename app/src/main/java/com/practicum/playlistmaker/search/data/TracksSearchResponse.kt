9ea4691dbea215d3ea4aeb786d437fbc2f170112
Дмитрий Харакшинов <d.kharakshinov@mail.ru>
2023-07-06 13:06:03 +0300

    Спринт 20. Добавил TrackDto.

--- app/src/main/java/com/practicum/playlistmaker/search/data/TracksSearchResponse.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/data/TracksSearchResponse.kt
@@ -1,7 +1,5 @@
 package com.practicum.playlistmaker.search.data
 
-import com.practicum.playlistmaker.search.domain.model.Track
-
 class TracksSearchResponse(
-    val results: List<Track>
+    val results: List<TrackDto>
 ) : Response()
